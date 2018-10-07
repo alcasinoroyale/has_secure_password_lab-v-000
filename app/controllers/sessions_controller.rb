e15a897d9c6be64b9700ffe85a6e7ef795e9cfc4
Alex Zdatny <alexzdatny@gmail.com>
2018-10-07 02:37:43 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        redirect_to users_path(@user)
+        redirect_to user_path(@user)
       else
         redirect_to login_path
     end
