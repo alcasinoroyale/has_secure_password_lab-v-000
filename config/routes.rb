8abc930cde193ddeca9ab36663d615313c0f7068
Alex Zdatny <alexzdatny@gmail.com>
2018-10-07 02:30:10 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :users, only: [:new, :create]
-  resources :sessions
+  resources :sessions, only: [:create, :destroy]
 end
