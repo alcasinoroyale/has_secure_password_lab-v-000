a640778cd29bf0c96fd78aae0a7cf355539d7c18
Alex Zdatny <alexzdatny@gmail.com>
2018-10-07 02:12:26 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user.create(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
   end
