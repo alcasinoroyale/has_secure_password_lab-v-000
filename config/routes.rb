98e56285be7133df3f303aee81ed1b70f3b8fe0c
Alex Zdatny <alexzdatny@gmail.com>
2018-10-07 02:36:46 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:create, :destroy]
 
   get '/login', to: 'sessions#new'
