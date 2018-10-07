c66654e5fe2276d556b349b7daa04d6bbd40548e
Alex Zdatny <alexzdatny@gmail.com>
2018-10-07 02:31:33 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to users_path(@user.id)
     else
-      redirect_to new_users_path
+      redirect_to new_user_path
     end
   end
 
